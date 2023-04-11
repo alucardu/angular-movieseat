2c14978d5f4b0241b9e12b41aa2048a05de4abed
Peter Boomsma <peter@cleanpixel.nl>
2023-04-11 16:09:45 +0200

    Created standalone youtube player component

--- src/app/components/features/movieDashboard/movie-dashboard.module.ts
+++ src/app/components/features/movieDashboard/movie-dashboard.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MovieDashboardRoutes } from './movie-dashboard.routing';
 import { MovieDetailsComponent } from '../movieDetails/movie-details/movie-details.component';
 import { MovieDashboardComponent } from './movie-dashboard/movie-dashboard.component';
-import { YouTubePlayerModule } from '@angular/youtube-player';
+import { YoutubePlayerComponent } from '../../shared/youtube-player/youtube-player.component';
 
 @NgModule({
   declarations: [MovieDashboardComponent, MovieDetailsComponent],
-  imports: [BrowserModule, MovieDashboardRoutes, YouTubePlayerModule],
-  providers: [],
-  bootstrap: [MovieDetailsComponent],
+  imports: [BrowserModule, MovieDashboardRoutes, YoutubePlayerComponent],
 })
 export class MovieDashboardModule {}
