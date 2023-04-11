ffb2d2034abb6176d5c951cd1a18a88572eb4770
Peter Boomsma <peter@cleanpixel.nl>
2023-04-11 16:46:10 +0200

    Added Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.movieseat.app',
+  appName: 'Movieseat',
+  webDir: 'dist/angular-movieseat',
+  bundledWebRuntime: false,
+};
+
+export default config;
