5f3837c770c93da434ce3b8354fa43ffddeb7dca
Peter Boomsma <peter@cleanpixel.nl>
2023-04-11 15:31:46 +0200

    Added the youtube player

--- src/app/components/features/movieDetails/movie-details/movie-details.component.ts
+++ src/app/components/features/movieDetails/movie-details/movie-details.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss'],
 })
-export class MovieDetailsComponent {}
+export class MovieDetailsComponent implements OnInit {
+  private apiLoaded = false;
+
+  public ngOnInit(): void {
+    if (!this.apiLoaded) {
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      this.apiLoaded = true;
+    }
+  }
+}
