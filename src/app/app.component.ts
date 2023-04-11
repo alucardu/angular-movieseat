159b83d27f9f1e4320f0cc470948f3b4f7758dc3
Peter Boomsma <peter@cleanpixel.nl>
2023-04-11 14:20:49 +0200

    Added angular material and only transition on mobile devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChildrenOutletContexts, OutletContext } from '@angular/router';
 import { slideInAnimation } from './animations';
+import {
+  BreakpointObserver,
+  BreakpointState,
+  Breakpoints,
+} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +13,29 @@ import { slideInAnimation } from './animations';
   styleUrls: ['./app.component.scss'],
   animations: [slideInAnimation],
 })
-export class AppComponent {
-  public title = 'angular-movieseat';
+export class AppComponent implements OnInit {
+  private showRouteAnimation = false;
 
-  public constructor(private contexts: ChildrenOutletContexts) {}
+  public constructor(
+    private breakpointObserver: BreakpointObserver,
+    private contexts: ChildrenOutletContexts
+  ) {}
+
+  public ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.HandsetPortrait, Breakpoints.Small])
+      .subscribe((state: BreakpointState) => {
+        this.showRouteAnimation = state.matches;
+      });
+  }
 
   public getRouteAnimationData(): OutletContext | null {
-    return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
-      'animation'
-    ];
+    if (this.showRouteAnimation) {
+      return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
+        'animation'
+      ];
+    } else {
+      return null;
+    }
   }
 }
