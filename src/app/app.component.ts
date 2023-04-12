2963eaff2de962cd9e93380b63773649765d1da1
Peter Boomsma <peter@cleanpixel.nl>
2023-04-12 11:24:44 +0200

    Youtube player goes "full screen" and page rotates when playing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChildrenOutletContexts, OutletContext } from '@angular/router';
 import { slideInAnimation } from './animations';
-import {
-  BreakpointObserver,
-  BreakpointState,
-  Breakpoints,
-} from '@angular/cdk/layout';
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
+import { App as CapacitorApp } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -16,24 +13,22 @@ import {
 export class AppComponent implements OnInit {
   private showRouteAnimation = false;
 
-  public constructor(
-    private breakpointObserver: BreakpointObserver,
-    private contexts: ChildrenOutletContexts
-  ) {}
+  public constructor(private breakpointObserver: BreakpointObserver, private contexts: ChildrenOutletContexts) {}
 
   public ngOnInit(): void {
-    this.breakpointObserver
-      .observe([Breakpoints.HandsetPortrait, Breakpoints.Small])
-      .subscribe((state: BreakpointState) => {
-        this.showRouteAnimation = state.matches;
-      });
+    CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+      if (window.screen.orientation.type === 'landscape-primary') return;
+      canGoBack ? window.history.back() : CapacitorApp.exitApp();
+    });
+
+    this.breakpointObserver.observe([Breakpoints.HandsetPortrait, Breakpoints.Small]).subscribe((state: BreakpointState) => {
+      this.showRouteAnimation = state.matches;
+    });
   }
 
   public getRouteAnimationData(): OutletContext | null {
     if (this.showRouteAnimation) {
-      return this.contexts.getContext('primary')?.route?.snapshot?.data?.[
-        'animation'
-      ];
+      return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
     } else {
       return null;
     }
