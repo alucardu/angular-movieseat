0f05504290be82365e29bb8b3b4e94072f824cf4
Peter Boomsma <peter@cleanpixel.nl>
2023-04-13 12:47:19 +0200

    Added a sidenav component

---
+++ src/app/components/UI/drawer/drawer.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ChildrenOutletContexts, OutletContext } from '@angular/router';
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
+import { App as CapacitorApp } from '@capacitor/app';
+import { slideInAnimation } from 'src/app/animations';
+
+@Component({
+  selector: 'app-drawer',
+  templateUrl: './drawer.component.html',
+  styleUrls: ['./drawer.component.scss'],
+  animations: [slideInAnimation],
+})
+export class DrawerComponent implements OnInit {
+  private showRouteAnimation = false;
+
+  public constructor(private breakpointObserver: BreakpointObserver, private contexts: ChildrenOutletContexts) {}
+
+  public ngOnInit(): void {
+    CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+      if (window.screen.orientation.type === 'landscape-primary') return;
+      canGoBack ? window.history.back() : CapacitorApp.exitApp();
+    });
+
+    this.breakpointObserver.observe([Breakpoints.HandsetPortrait, Breakpoints.Small]).subscribe((state: BreakpointState) => {
+      this.showRouteAnimation = state.matches;
+    });
+  }
+
+  public getRouteAnimationData(): OutletContext | null {
+    if (this.showRouteAnimation) {
+      return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+    } else {
+      return null;
+    }
+  }
+}
