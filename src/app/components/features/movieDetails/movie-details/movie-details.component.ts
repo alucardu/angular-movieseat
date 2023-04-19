f0f6458c4904d63e524839a73816a394331eb9d4
Peter Boomsma <peter@cleanpixel.nl>
2023-04-19 21:15:07 +0200

    Fixed bug with animation

--- src/app/components/features/movieDetails/movie-details/movie-details.component.ts
+++ src/app/components/features/movieDetails/movie-details/movie-details.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss'],
 })
 export class MovieDetailsComponent implements OnInit {
+  @ViewChild('youtubeContainer', { static: true}) private youtubeContainer!: ElementRef<HTMLElement>
   private apiLoaded = false;
 
   public ngOnInit(): void {
     if (!this.apiLoaded) {
       const tag = document.createElement('script');
       tag.src = 'https://www.youtube.com/iframe_api';
-      document.body.appendChild(tag);
+
+      this.youtubeContainer.nativeElement.appendChild(tag)
       this.apiLoaded = true;
     }
   }
