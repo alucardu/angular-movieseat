7e9694f0f7768715fe60270d75c8e212f15186e9
Peter Boomsma <peter@cleanpixel.nl>
2023-04-25 13:28:35 +0200

    created movie rating component

--- src/app/components/features/movie-dashboard/movie-dashboard.module.ts
+++ src/app/components/features/movie-dashboard/movie-dashboard.module.ts
@@ -5,12 +5,13 @@ import { MovieDashboardRoutes } from './movie-dashboard.routing';
 import { MovieDetailsComponent } from '../movie-details/movie-details/movie-details.component';
 import { MovieDashboardComponent } from './movie-dashboard/movie-dashboard.component';
 import { YoutubePlayerComponent } from '../../shared/youtube-player/youtube-player.component';
-import { MovieDisplayComponent } from '../movie-display/movie-display.component';
+import { MovieDisplayComponent } from './movie-display/movie-display.component';
 import { MaterialModule } from 'src/app/material.module';
 import { ClipsContainerComponent } from '../movie-details/clips-container/clips-container.component';
 import { ImageSliderComponent } from '../movie-details/image-slider/image-slider.component';
 import { MovieReviewsComponent } from '../movie-details/movie-reviews/movie-reviews.component';
 import { MovieReviewComponent } from '../movie-details/movie-reviews/movie-review/movie-review.component';
+import { MovieRatingComponent } from '../movie-details/movie-rating/movie-rating.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { MovieReviewComponent } from '../movie-details/movie-reviews/movie-revie
     ClipsContainerComponent,
     MovieReviewsComponent,
     MovieReviewComponent,
-    ImageSliderComponent
+    ImageSliderComponent,
+    MovieRatingComponent
   ],
   imports: [
     BrowserModule,
