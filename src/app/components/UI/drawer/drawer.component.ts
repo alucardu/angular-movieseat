95195ab02e7346502ca8e4c27ca110a57c390bd2
Peter Boomsma <peter@cleanpixel.nl>
2023-08-12 19:39:31 +0200

    Fixed Material rendering issue

--- src/app/components/UI/drawer/drawer.component.ts
+++ src/app/components/UI/drawer/drawer.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, NgZone, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { ChildrenOutletContexts, OutletContext, RouterModule, } from '@angular/router';
 import { App as CapacitorApp } from '@capacitor/app';
 import { ScrollService } from 'src/app/services/scroll.service';
@@ -43,7 +43,6 @@ export class DrawerComponent implements OnInit, AfterViewInit {
         this.exitConfirmation = true;
       };
     });
-
   }
 
   public ngAfterViewInit(): void {
--- src/app/components/features/movie-search/movie-search.component.ts
+++ src/app/components/features/movie-search/movie-search.component.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MaterialModule } from 'src/app/material.module';
 import { MovieSearchResultComponent } from './movie-search-result/movie-search-result.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie-search',
   templateUrl: './movie-search.component.html',
   styleUrls: ['./movie-search.component.scss'],
   standalone: true,
-  imports: [MaterialModule]
+  imports: [CommonModule, MaterialModule]
 })
 export class MovieSearchComponent {
-  public constructor(public dialog: MatDialog) {}
+  public constructor(
+    private dialog: MatDialog,
+    private ngZone: NgZone
+  ) {}
 
   public movieSearchDialog(): void {
-    this.dialog.open(MovieSearchDialogComponent, {
-      height: '98vh',
-      minWidth: '95vw',
-    });
+    this.ngZone.run(() => {
+      this.dialog.open(MovieSearchDialogComponent, {
+        height: '98vh',
+        minWidth: '95vw',
+      });
+    })
   }
 }
 
@@ -25,7 +31,7 @@ export class MovieSearchComponent {
   templateUrl: 'movie-search-dialog/movie-search-dialog.html',
   styleUrls: ['movie-search-dialog/movie-search-dialog.scss'],
   standalone: true,
-  imports: [MaterialModule, MovieSearchResultComponent]
+  imports: [CommonModule, MaterialModule, MovieSearchResultComponent]
 })
 
 export class MovieSearchDialogComponent {
