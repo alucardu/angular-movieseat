3f385fed848df0338e3aba81648bff6a32dee892
Peter Boomsma <peter@cleanpixel.nl>
2023-08-28 14:43:56 +0200

    updated watchlist to show tags

--- src/app/components/features/watchlist/watchlist.component.ts
+++ src/app/components/features/watchlist/watchlist.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { watchlist } from 'src/app/mock/watchlist.json';
 import { CommonModule } from '@angular/common';
 import { MovieDisplayComponent } from '../movie-dashboard/movie-display/movie-display.component';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-watchlist',
@@ -10,6 +11,25 @@ import { MovieDisplayComponent } from '../movie-dashboard/movie-display/movie-di
   standalone: true,
   imports: [CommonModule, MovieDisplayComponent]
 })
-export class WatchlistComponent {
+export class WatchlistComponent implements OnInit {
+  private metaTagService = inject(Meta)
+  private metaTitleService = inject(Title)
+
   public watchlist = watchlist
+
+  public ngOnInit(): void {
+    this.metaTagService.addTags([
+      {
+        name: 'keywords',
+        content: 'Angular SEO Integration, Music CRUD, Angular Universal',
+      },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Digamber Singh' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' },
+    ]);
+
+    this.metaTitleService.setTitle('Movie title goes here')
+  }
 }
