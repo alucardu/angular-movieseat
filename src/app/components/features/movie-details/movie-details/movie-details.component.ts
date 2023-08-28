2b41fc0b72fad5032e37d3d676399ffee470a29f
Peter Boomsma <peter@cleanpixel.nl>
2023-08-28 21:24:09 +0200

    debug

--- src/app/components/features/movie-details/movie-details/movie-details.component.ts
+++ src/app/components/features/movie-details/movie-details/movie-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './movie-details.component.html',
@@ -6,6 +7,25 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: []
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+  private metaTagService = inject(Meta)
+  private metaTitleService = inject(Title)
+
+  public ngOnInit(): void {
+    this.metaTagService.addTags([
+      {
+        name: 'keywords',
+        content: 'Angular SEO Integration, Music CRUD, Angular Universal',
+      },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Digamber Singh' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' },
+    ]);
+
+    this.metaTitleService.setTitle('Movie title goes here')
+  }
+
 
 }
