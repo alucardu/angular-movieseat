92f6665489c11b6903bf28ea317dfcb372b2f7d0
Peter Boomsma <peter@cleanpixel.nl>
2023-08-30 13:26:33 +0200

    Change capacitor webDir

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.movieseat.app',
   appName: 'Movieseat',
-  webDir: 'dist/angular-movieseat',
+  webDir: 'dist/angular-movieseat/browser',
   plugins: {
     SplashScreen: {
       launchShowDuration: 600,
