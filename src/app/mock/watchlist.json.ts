0b9062e192c249288212d62fd6272f2509b096a6
Peter Boomsma <peter@cleanpixel.nl>
2023-09-04 12:51:08 +0200

    Changed SSR movie image

--- src/app/mock/watchlist.json.ts
+++ src/app/mock/watchlist.json.ts
@@ -2,6 +2,7 @@ export interface IMovie {
   id: number
   title: string
   poster: string
+  backdrop: string
   release_date: string
   release_year: string,
   overview: string
@@ -21,6 +22,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Moonrise Kingdom",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
+    backdrop: "https://www.themoviedb.org/t/p/original/xwrYBIt2BV1KlPRaAOLPmUqM6p3.jpg",
     release_date: "05-25-2012",
     release_year: '2012',
     overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
@@ -35,6 +37,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Oppenheimer",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    backdrop: 'https://www.themoviedb.org/t/p/original/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg',
     release_date: "07-21-2023",
     release_year: '2023',
     overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
@@ -49,6 +52,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Oppenheimer",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    backdrop: 'https://www.themoviedb.org/t/p/original/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg',
     release_date: "07-21-2023",
     release_year: '2023',
     overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
@@ -63,6 +67,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Moonrise Kingdom",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
+    backdrop: "https://www.themoviedb.org/t/p/original/xwrYBIt2BV1KlPRaAOLPmUqM6p3.jpg",
     release_date: "05-25-2012",
     release_year: '2012',
     overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
@@ -77,6 +82,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Moonrise Kingdom",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
+    backdrop: "https://www.themoviedb.org/t/p/original/xwrYBIt2BV1KlPRaAOLPmUqM6p3.jpg",
     release_date: "05-25-2012",
     release_year: '2012',
     overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
@@ -91,6 +97,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Oppenheimer",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    backdrop: 'https://www.themoviedb.org/t/p/original/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg',
     release_date: "07-21-2023",
     release_year: '2023',
     overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
@@ -105,6 +112,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Oppenheimer",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    backdrop: 'https://www.themoviedb.org/t/p/original/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg',
     release_date: "07-21-2023",
     release_year: '2023',
     overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
@@ -119,63 +127,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Moonrise Kingdom",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
-    release_date: "05-25-2012",
-    release_year: '2012',
-    overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
-    tag: 'A tormenting and surprising story of children and adults during the stormy days of the summer of 1965.',
-    id: 1,
-    clips: [{ id: 'RBCRe73dZb0' }, { id: 'qtnq_Ffieno' }, { id: '_eOI3AamSm8' }, { id: 'uDph9wSeWvY' }],
-    tags: ['Comedy', 'Drama', 'Romance'],
-    writers: ['Wes Anderson', 'Roman Coppola'],
-    directors: ['Wes Anderson'],
-    runtime: '1h34m'
-  },
-
-  {
-    title: "Moonrise Kingdom",
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
-    release_date: "05-25-2012",
-    release_year: '2012',
-    overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
-    tag: 'A tormenting and surprising story of children and adults during the stormy days of the summer of 1965.',
-    id: 1,
-    clips: [{ id: 'RBCRe73dZb0' }, { id: 'qtnq_Ffieno' }, { id: '_eOI3AamSm8' }, { id: 'uDph9wSeWvY' }],
-    tags: ['Comedy', 'Drama', 'Romance'],
-    writers: ['Wes Anderson', 'Roman Coppola'],
-    directors: ['Wes Anderson'],
-    runtime: '1h34m'
-  },
-  {
-    title: "Oppenheimer",
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
-    release_date: "07-21-2023",
-    release_year: '2023',
-    overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
-    tag: 'The world forever changes.',
-    id: 2,
-    clips: [{ id: 'uYPbbksJxIg' }, { id: 'bK6ldnjE3Y0' }, { id: 'sOsIKu2VAkM' }, { id: 'yi363I5MoAk' }],
-    tags: ['Thriller', 'Drama', 'Horror'],
-    writers: ['Christopher Nolan'],
-    directors: ['Christopher Nolan'],
-    runtime: '3h1m'
-  },
-  {
-    title: "Oppenheimer",
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
-    release_date: "07-21-2023",
-    release_year: '2023',
-    overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
-    tag: 'The world forever changes.',
-    id: 2,
-    clips: [{ id: 'uYPbbksJxIg' }, { id: 'bK6ldnjE3Y0' }, { id: 'sOsIKu2VAkM' }, { id: 'yi363I5MoAk' }],
-    tags: ['Thriller', 'Drama', 'Horror'],
-    writers: ['Christopher Nolan'],
-    directors: ['Christopher Nolan'],
-    runtime: '3h1m'
-  },
-  {
-    title: "Moonrise Kingdom",
-    poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
+    backdrop: "https://www.themoviedb.org/t/p/original/xwrYBIt2BV1KlPRaAOLPmUqM6p3.jpg",
     release_date: "05-25-2012",
     release_year: '2012',
     overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
@@ -191,6 +143,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Moonrise Kingdom",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
+    backdrop: "https://www.themoviedb.org/t/p/original/xwrYBIt2BV1KlPRaAOLPmUqM6p3.jpg",
     release_date: "05-25-2012",
     release_year: '2012',
     overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
@@ -205,6 +158,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Oppenheimer",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    backdrop: 'https://www.themoviedb.org/t/p/original/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg',
     release_date: "07-21-2023",
     release_year: '2023',
     overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
@@ -219,6 +173,7 @@ export const watchlist: IMovie[] = [
   {
     title: "Oppenheimer",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    backdrop: 'https://www.themoviedb.org/t/p/original/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg',
     release_date: "07-21-2023",
     release_year: '2023',
     overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
@@ -233,6 +188,68 @@ export const watchlist: IMovie[] = [
   {
     title: "Moonrise Kingdom",
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
+    backdrop: "https://www.themoviedb.org/t/p/original/xwrYBIt2BV1KlPRaAOLPmUqM6p3.jpg",
+    release_date: "05-25-2012",
+    release_year: '2012',
+    overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
+    tag: 'A tormenting and surprising story of children and adults during the stormy days of the summer of 1965.',
+    id: 1,
+    clips: [{ id: 'RBCRe73dZb0' }, { id: 'qtnq_Ffieno' }, { id: '_eOI3AamSm8' }, { id: 'uDph9wSeWvY' }],
+    tags: ['Comedy', 'Drama', 'Romance'],
+    writers: ['Wes Anderson', 'Roman Coppola'],
+    directors: ['Wes Anderson'],
+    runtime: '1h34m'
+  },
+
+  {
+    title: "Moonrise Kingdom",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
+    backdrop: "https://www.themoviedb.org/t/p/original/xwrYBIt2BV1KlPRaAOLPmUqM6p3.jpg",
+    release_date: "05-25-2012",
+    release_year: '2012',
+    overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
+    tag: 'A tormenting and surprising story of children and adults during the stormy days of the summer of 1965.',
+    id: 1,
+    clips: [{ id: 'RBCRe73dZb0' }, { id: 'qtnq_Ffieno' }, { id: '_eOI3AamSm8' }, { id: 'uDph9wSeWvY' }],
+    tags: ['Comedy', 'Drama', 'Romance'],
+    writers: ['Wes Anderson', 'Roman Coppola'],
+    directors: ['Wes Anderson'],
+    runtime: '1h34m'
+  },
+  {
+    title: "Oppenheimer",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    backdrop: 'https://www.themoviedb.org/t/p/original/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg',
+    release_date: "07-21-2023",
+    release_year: '2023',
+    overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
+    tag: 'The world forever changes.',
+    id: 2,
+    clips: [{ id: 'uYPbbksJxIg' }, { id: 'bK6ldnjE3Y0' }, { id: 'sOsIKu2VAkM' }, { id: 'yi363I5MoAk' }],
+    tags: ['Thriller', 'Drama', 'Horror'],
+    writers: ['Christopher Nolan'],
+    directors: ['Christopher Nolan'],
+    runtime: '3h1m'
+  },
+  {
+    title: "Oppenheimer",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    backdrop: 'https://www.themoviedb.org/t/p/original/rLb2cwF3Pazuxaj0sRXQ037tGI1.jpg',
+    release_date: "07-21-2023",
+    release_year: '2023',
+    overview: 'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
+    tag: 'The world forever changes.',
+    id: 2,
+    clips: [{ id: 'uYPbbksJxIg' }, { id: 'bK6ldnjE3Y0' }, { id: 'sOsIKu2VAkM' }, { id: 'yi363I5MoAk' }],
+    tags: ['Thriller', 'Drama', 'Horror'],
+    writers: ['Christopher Nolan'],
+    directors: ['Christopher Nolan'],
+    runtime: '3h1m'
+  },
+  {
+    title: "Moonrise Kingdom",
+    poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xrziXRHRQ7c7YLIehgSJY8GQBsx.jpg",
+    backdrop: "https://www.themoviedb.org/t/p/original/xwrYBIt2BV1KlPRaAOLPmUqM6p3.jpg",
     release_date: "05-25-2012",
     release_year: '2012',
     overview: 'Set on an island off the coast of New England in the summer of 1965, Moonrise Kingdom tells the story of two twelve-year-olds who fall in love, make a secret pact, and run away together into the wilderness. As various authorities try to hunt them down, a violent storm is brewing off-shore – and the peaceful island community is turned upside down in more ways than anyone can handle.',
