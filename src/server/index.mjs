184a14df0c122ecb179ea7ef55590e6530ee624e
Peter Boomsma <peter@cleanpixel.nl>
2023-09-23 16:10:58 +0200

    update server

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -40,7 +40,7 @@ const server = new ApolloServer({
 await server.start();
 
 const corsOptions = {
-  origin: true,
+  origin: ["https://www.moviese.at"],
   optionsSuccessStatus: 200,
   credentials: true
 };
