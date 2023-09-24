7810389a70ebe0eb3547f49b6001b27461a675e1
Peter Boomsma <peter@cleanpixel.nl>
2023-09-24 12:47:21 +0200

    update

--- src/server/resolvers/user.mjs
+++ src/server/resolvers/user.mjs
@@ -24,7 +24,7 @@ const userResolvers = {
 
         const tokens = setTokens(user)
 
-        res.cookie('authToken', tokens.accessToken, { maxAge: 3600000, httpOnly: true, secure: true });
+        res.cookie('authToken', tokens.accessToken, { maxAge: 3600000, httpOnly: true, secure: true, SameSite: 'none' });
 
         return {
           data: user,
