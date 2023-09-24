2a9a8641f5577932d3d92ee894d33fcbd3e7d1a3
Peter Boomsma <peter@cleanpixel.nl>
2023-09-24 12:27:56 +0200

    updated server for cors

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -40,7 +40,7 @@ const server = new ApolloServer({
 await server.start();
 
 const corsOptions = {
-  origin: true,
+  origin: ["http://localhost", "http://www.moviese.at"],
   optionsSuccessStatus: 200,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
